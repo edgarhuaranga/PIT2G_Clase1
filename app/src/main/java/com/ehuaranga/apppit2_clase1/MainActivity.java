6a5b40f8c0071c45776147d6b87ccfd0dcc4ebd7
hued <edhu1227@gmail.com>
2018-04-21 11:40:06 -0500

    Java modificado

--- app/src/main/java/com/ehuaranga/apppit2_clase1/MainActivity.java
+++ app/src/main/java/com/ehuaranga/apppit2_clase1/MainActivity.java
@@ -2,9 +2,11 @@ package com.ehuaranga.apppit2_clase1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +19,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textViewSaludo = findViewById(R.id.textview_saludo);
+        editTextNombre = findViewById(R.id.edittext_nombre);
+        buttonSaludar = findViewById(R.id.button_saludar);
 
 
     }
+
+    public void Saludar(View view){
+        Toast.makeText(this, "Has presionado el boton de  saludar",Toast.LENGTH_SHORT).show();
+    }
+
 }
