07286c0a4393c1bc7333dbb2d7f0aa4ec908598c
hued <edhu1227@gmail.com>
2018-04-21 11:25:35 -0500

    Agregando ids

--- app/src/main/java/com/ehuaranga/apppit2_clase1/MainActivity.java
+++ app/src/main/java/com/ehuaranga/apppit2_clase1/MainActivity.java
@@ -2,12 +2,22 @@ package com.ehuaranga.apppit2_clase1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewSaludo;
+    EditText editTextNombre;
+    Button buttonSaludar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
