95a5010b473d71a92206fd2e0a43b16a6cc37d42
hued <edhu1227@gmail.com>
2018-04-21 11:12:08 -0500

    Initial commit

---
+++ app/src/main/java/com/ehuaranga/apppit2_clase1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ehuaranga.apppit2_clase1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
